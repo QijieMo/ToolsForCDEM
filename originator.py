1576c18a6dcd7632a26c6a8527451847f9404d20
demirsam <sametdemir@gmail.com>
2018-07-20 16:48:19 +0300

    A tool to move xyz to origin

---
+++ originator.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+import numpy as np
+from pprint import pprint as pp
+import sys
+
+np.set_printoptions(suppress=True)
+np.set_printoptions(precision=10)
+
+if len(sys.argv)<2 or len(sys.argv)>2:
+    print "Arguments are wrong.\n" \
+          "Correct usage: originator.py xyz_name"
+    exit(0)
+else:
+    xyz_name = sys.argv[1]
+
+xyz_file = open(xyz_name,"r").readlines()[2:]
+
+a = np.zeros(len(xyz_file)*3).reshape(len(xyz_file),3)
+atom_names = []
+
+for i in range(len(xyz_file)):
+	xyz_file[i] = xyz_file[i].rstrip("\n")
+	xyz_file[i] = xyz_file[i].split()
+	atom_names.append(xyz_file[i][0])
+	a[i][0] = xyz_file[i][1]
+	a[i][1] = xyz_file[i][2]
+	a[i][2] = xyz_file[i][3]
+
+ref_at = [0.,0.,0.]
+ref_at[:] = a[0]
+print ref_at
+
+for i in range(len(a)):
+	a[i] = a[i] - ref_at
+	print ref_at
+
+print a
+#for i in range(len(a)):
+#  print a[i][0],"\t",a[i][1],"\t",a[i][2]
+
+print atom_names
