6c4e9d4472f345fbfcdfe68cda6b68953dea1e68
Mulayim <sametdemir@gmail.com>
2018-12-04 18:54:41 +0300

    Order was not working correctky, now fixed

--- fcaspesa_analyser_withsym.py
+++ fcaspesa_analyser_withsym.py
@@ -64,7 +64,7 @@ for i in xrange(len(xyz_adress)):
 	#Getting energy
 	get_energy_fcaspesa(xyzfolder_path,name_without_xyz,energy_dict)
 
-energy_sorted = sorted(energy_dict.items(), key=lambda t: t[1], reverse=True)
+energy_sorted = sorted(energy_dict.items(), key=lambda t: t[1])
 
 table = PrettyTable([
 "Address",
@@ -75,6 +75,7 @@ table = PrettyTable([
 ])
 #Finding symmetries and creating cif files
 base_directory = cmdline("pwd").rstrip("\n")
+import time
 for i in xrange(len(energy_sorted)):
 	print("Processing "+str(i)+" of "+ str(len(energy_sorted))
 		+" name-> "+ energy_sorted[i][0])
@@ -89,6 +90,7 @@ for i in xrange(len(energy_sorted)):
 		work_path_processed += work_path_raw[j]+"/"
 	#now i can change working directory and execute the xyz2cif
 	os.chdir(work_path_processed)
+	start = time.time()
 	if(float(energy_sorted[i][1])!=0.):
 		sym_res = cmdline("xyz2cif.py "+xyz_file_name+".xyz cell_file "+sym_tol+" "
 			+xyz_file_name+".cif").rstrip("\n")
@@ -101,11 +103,13 @@ for i in xrange(len(energy_sorted)):
 				sym_res = "Space must be planar or problematic, sym wont work"
 	else:
 		sym_res = "E=0,wont look for sym"
+	print sym_res,(time.time() - start)
 	volume = cmdline("get_volume.py cell_file").rstrip("\n") + " Ang^3"
-	table.add_row([energy_sorted[i][0],energy_sorted[i][1],volume,str(dummy_tol),sym_res.rstrip(" ")])
+	table.add_row([energy_sorted[i][0],float(energy_sorted[i][1]),volume,str(dummy_tol),sym_res.rstrip(" ")])
 	os.chdir(base_directory)
 
-print(table)
+#table.sortby = "Energy"
+print table
 
 Analysis_file = open(Analysis_dir+".txt","w")
 Analysis_file.write(str(table)+"\n")
